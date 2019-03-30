4e62d4edc1494063dbd643fdcc6c9b3075c26d3b
Sean <30637155+Snipet@users.noreply.github.com>
2019-03-30 17:02:20 -0400

    Added the Launcher

---
+++ scratchBot_Launcher.py
@@ -0,0 +1,45 @@
+import scratchapi
+import datetime
+import requests
+import json
+import random
+import time
+import sys
+from bs4 import BeautifulSoup
+import scratchCommands
+global last
+
+def update():
+    print("Getting most recent version...")
+    info = requests.get("https://api.github.com/repos/Snipet/scratchBot/contents/")
+    info = json.loads(info.text)
+    getFile("https://raw.githubusercontent.com/Snipet/scratchBot/master/scratchCommands.py", "scratchCommands.py")
+    f = open("scratchCommands.py", "r")
+    code = f.read()
+    f.close()
+    print("\nDownloaded ScratchBot")
+    print("File Size: " + str(info[3]["size"]))
+
+def getFile(URL, FILE):
+    global lines
+    global online
+    r = requests.get(URL)
+    online = r.text
+    code = r.text
+    code = code.split("\n")
+    lines = []
+    for line in code:
+        lines.append(line.replace("\r", ""))
+    open(FILE, 'w').close()
+    f = open(FILE, "a")
+    for line in lines:
+        f.write(line + "\n")
+    f.close()
+
+update()
+print("\nStarting... ")
+
+
+while True:
+    scratchCommands.scratchCheck("-Snipet_Test", "-Snipet-", "seanfunk")
+    time.sleep(5)
