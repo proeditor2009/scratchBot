152243804c17f47d470fa04a04465824035baf44
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-04-01 20:00:42 +0300

    Remove uneccessary imports

--- scratchBot_Launcher.py
+++ scratchBot_Launcher.py
@@ -1,25 +1,17 @@
-import scratchapi
-import datetime
 import requests
 import json
-import random
 import time
 import sys
-from bs4 import BeautifulSoup
 import scratchCommands
-global last
 
 def update():
     print("Getting most recent version...")
-    info = requests.get("https://api.github.com/repos/Snipet/scratchBot/contents/")
+    info = requests.get("https://api.github.com/repos/BonfireScratch/scratchBot/contents/")
     info = json.loads(info.text)
-    getFile("https://raw.githubusercontent.com/Snipet/scratchBot/master/scratchCommands.py", "scratchCommands.py")
+    getFile("https://raw.githubusercontent.com/BonfireScratch/scratchBot/master/scratchCommands.py", "scratchCommands.py")
     print("ScratchBot has been downloaded")
-    print("File Size: " + str(info[4]["size"]))
 
 def getFile(URL, FILE):
-    global lines
-    global online
     r = requests.get(URL)
     online = r.text
     code = r.text
