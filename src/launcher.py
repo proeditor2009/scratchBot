ea9083e51acf568955b3c3ccc7e3e715406c5c46
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-05-26 10:36:02 +0300

    Update launcher.py

--- src/launcher.py
+++ src/launcher.py
@@ -1,15 +1,12 @@
 import requests
 import scratchapi
 import json
+import encryptor
 import sys
 
 global mostRecentVersion
 global version
-
-def update(VERSION):
-    print("Downloading most recent version (" + VERSION + ")")
-    getFile("https://raw.githubusercontent.com/Snipet/scratchBot/master/src/scratchCommands.py", "scratchCommands.py")
-    setVersion(mostRecentVersion)
+global j
 
 def getFile(URL, FILE):
     lines = getFileContents(URL)
@@ -27,40 +24,34 @@ def getFileContents(URL):
     for line in code:
         lines.append(line.replace("\r", ""))
     return lines
-  
-def setVersion(v):
-    j = {
-        "version": v
-    }
-    
-    with open("version.json", "w") as f:
-        f.write(json.dumps(j))
-    
+
 def askForData():
-    isValid = 0
-    while isValid != 1:
+    isValid = False
+
+    while not isValid:
         user = input("Enter the bot's username > ")
         pas = input("Enter the bot's password > ")
         try:
             scratch = scratchapi.ScratchUserSession(user, pas)
-            isValid = 1
+            isValid = True
         except:
             print("Invalid username or password")
     
-    j = {
-        "version": mostRecentVersion,
-        "username": user,
-        "password": pas
-    }
+    j["username"] = user
+    j["password"] = str(encryptor.encrypt(pas), 'utf-8')
     
     with open("version.json", "w") as f:
         f.write(json.dumps(j))
     
 def main():
+    mostRecentVersion = getFileContents("https://raw.githubusercontent.com/Snipet/scratchBot/master/version.txt")[0]
+
     try:
         with open("version.json", "r") as f:
             data = json.loads(f.read())
             version = data["version"]
+            username = data["username"]
+            pas = data["password"]
     except:
         version = None
         
@@ -68,12 +59,18 @@ def main():
         if version:
             inp = str(input("A new version of ScratchBot is available. Would you like to download it? > (yes/no) "))
             if inp == "yes":
-                update(mostRecentVersion)
+                print("Downloading most recent version (" + mostRecentVersion + ")")
+                getFile("https://raw.githubusercontent.com/Snipet/scratchBot/master/src/scratchCommands.py", "scratchCommands.py")
+                
+                j = {"version": mostRecentVersion, "username": username, "password": pas}
+
+                with open("version.json", "w") as f:
+                    f.write(json.dumps(j))
+                
                 print("ScratchBot has been updated")
         else:
-            update(mostRecentVersion)
+            j["version"] = mostRecentVersion
             askForData()
-            print("ScratchBot has been downloaded")
 
-mostRecentVersion = getFileContents("https://raw.githubusercontent.com/Snipet/scratchBot/master/version.txt")[0]
+j = {}
 main()
